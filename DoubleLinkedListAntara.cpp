5e8133574ba306f69f7ad707ff21317b5f41e0a6
vannn-tech <jovan20166@gmail.com>
2026-08-06 10:19:54 +0700

    Kontrol alur aplikasi

--- DoubleLinkedListAntara.cpp
+++ DoubleLinkedListAntara.cpp
@@ -252,3 +252,10 @@ int main()
         default:
             cout << "Invalid option\n";
         }
+        cout << "\nPress Enter to continue...";
+        cin.ignore();
+        cin.get();
+        cout << endl;
+        system("clear");
+    } while (choice != '6');
+}
